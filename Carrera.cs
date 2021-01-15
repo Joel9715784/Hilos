f77feec080b4ce9fb61e2fef5306f73c09f278a3
Joel9715784 <77417721+Joel9715784@users.noreply.github.com>
2021-01-15 13:21:27 -0500

    Tarea sobre hilos
    Tarea sobre hilos

---
+++ Carrera.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Hiloscarrea;
+namespace Hilos
+{
+    class Carrera
+    {
+        public  static void Competidor(object obj)
+        {
+            var nombre = (string)obj;
+
+            var hiloInicial = Thread.CurrentThread;
+            var sanco = 0;
+            var random = new Random();
+
+            while ( sanco < 10000 )
+            {
+
+                sanco += random.Next(0, 4);
+                Console.WriteLine("Competidor {0} dio {1} sancos", nombre, sanco);
+                Thread.Sleep(10);
+
+
+            }
+            Console.WriteLine("Comptidor  {0} llego primero ",nombre); // el resto son lentos XD
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Hiloscarera;
+
+namespace Hilos
+{
+    class Program
+    {
+         public static void Main(string[] args)
+        {
+            Console.WriteLine("PAIRIKI");
+           
+
+            Thread competidor1 = new Thread(Carrera.Competidor);
+            Thread competidor2 = new Thread(Carrera.Competidor);
+            Thread competidor3= new Thread(Carrera.Competidor);
+            Thread competidor4= new Thread(Carrera.Competidor);
+
+            competidor1.Start();
+            competidor2.Start();
+            competidor3.Start();
+            competidor4.Start();
+
+            competidor1.Join();
+            competidor2.Join();
+            competidor3.Join();
+            competidor4.Join();
+
+
+
+        }
+
+        static void Ejecturar()
+        {
+            
+
+            var hiloInicial = Thread.CurrentThread; 
+            hiloInicial.CurrentCulture = new System.Globalization.CultureInfo("en-US"); 
+            Console.WriteLine("Hilo en curso {0}: ", hiloInicial.ManagedThreadId); 
+
+            Console.WriteLine("Mi cultura es  {0} ", hiloInicial.CurrentCulture); 
+
+            var random = new Random();
+            for (int i =0; i <= 10; i++)
+            {
+                Console.WriteLine("HILO {0} ix {1}", hiloInicial.ManagedThreadId, i);
+
+                Thread.Sleep(random.Next(500,2000));
+            }
+
+        }
+    }
+}
